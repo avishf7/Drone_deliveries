8d3b76ce82161d9defa426945708058063ddd8dc
chaim-gootwain <e77050@gmail.com>
2021-10-05 19:56:53 +0300

    update205459738

---
+++ targil0/Program9738.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+             Welcome9738();
+            Console.ReadKey();
+        }
+
+        private static void Welcome9738()
+        {
+            Console.WriteLine("Enter your name: ");
+            string s = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", s);
+        }
+
+        static partial void Welcome8790();
+
+    }
+}
