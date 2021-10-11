b1183ccd988457e59189229753b96df8ff43d8d6
avishay farkash <62483176+avishf7@users.noreply.github.com>
2021-10-11 15:03:36 +0300

    ex 1.1

---
+++ ConsoleUI/MenuOptions.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    /// <summary>
+    /// MenuOptions Class is an auxiliary class 
+    /// for printing a selection menu on the console
+    /// </summary>
+    class MenuOptions
+    {
+        public enum OpeningOptions
+        {
+            EXIT, ADD, UPDATE, PRINT, PRINT_LISTS,
+            FIRST_MENU_OPTION = EXIT, LAST_MENU_OPTION = PRINT_LISTS, DEFAULT
+        };
+
+        public enum InsertOptions
+        {
+            BACK, STATION, DRONE, CUSTOMER, PACKAGE,
+            FIRST_MENU_OPTION = BACK, LAST_MENU_OPTION = PACKAGE, DEFAULT
+        };
+
+        public enum UpdateOptions
+        {
+            BACK, ASSOCIATION, PICKING_UP, SUPPLY, CHARGING, RELEASE,
+            FIRST_MENU_OPTION = BACK, LAST_MENU_OPTION = RELEASE, DEFAULT
+        };
+
+        public enum DisplayOptions
+        {
+            BACK, STATION, DRONE, CUSTOMER, PACKAGE, 
+            FIRST_MENU_OPTION = BACK, LAST_MENU_OPTION = PACKAGE, DEFAULT
+        };
+
+        public enum ListViewOptions
+        {
+            BACK, STATIONS, DRONES, CUSTOMERS, PACKAGES, UNASSIGNED_STATIONS, AVAILABLE_FOR_CHARGING,
+            FIRST_MENU_OPTION = BACK, LAST_MENU_OPTION = AVAILABLE_FOR_CHARGING, DEFAULT
+        };
+
+
+        private static string[] OpeningMenuOptionLine = new string[(int)OpeningOptions.LAST_MENU_OPTION + 1]
+        {
+            "Exit",
+            "Insert options",
+            "Update Options",
+            "Display Options",
+            "List View Options"
+        };
+
+        private static string[] InsertMenuOptionLine = new string[(int)InsertOptions.LAST_MENU_OPTION + 1]
+        {
+            "Back to the main menu",
+            "Add station",
+            "Add drone",
+            "Add customer",
+            "Add package"
+        };
+
+        private static string[] UpdateMenuOptionLine = new string[(int)UpdateOptions.LAST_MENU_OPTION + 1]
+        {
+            "Back to the main menu",
+            "Assigning a Package to a Drone",
+            "Pickup of a package by a drone",
+            "End of supply",
+            "Sending a drone for charging at a base station",
+            "Releasing a Drone from a Charger",
+        };
+
+        private static string[] DispalyMenuOptionLine = new string[(int)DisplayOptions.LAST_MENU_OPTION + 1]
+        {
+            "Back to the main menu",
+            "Show station",
+            "Show drone",
+            "Show customer",
+            "Show package"
+        };
+
+        private static string[] ListViewMenuOptionLine = new string[(int)ListViewOptions.LAST_MENU_OPTION + 1]
+       {
+            "Back to the main menu",
+            "Show a list of stations",
+            "Show the list of drones",
+            "Show customer list",
+            "Show the list of packages",
+            "Show a list of packages that have not yet been assigned to the drone",
+            "Show of stations with available charging stations",
+       };
+
+
+        public static OpeningOptions PrintOpeningMenu()
+        {
+            OpeningOptions chosen;
+
+            Console.WriteLine("Menu options:\n");
+
+            for (chosen = OpeningOptions.FIRST_MENU_OPTION; chosen <= OpeningOptions.LAST_MENU_OPTION; ++chosen)
+                Console.WriteLine("{0,10}\t--\t" + OpeningMenuOptionLine[(int)chosen], (int)chosen);
+
+            Console.Write("\nPlease choose a menu option: ");
+            if (int.TryParse(Console.ReadLine(), out int s))
+                chosen = (OpeningOptions)s;
+            else
+                throw new Exception();
+
+            return chosen;
+        }
+
+        public static InsertOptions PrintInsertMenu()
+        {
+            InsertOptions chosen;
+
+            Console.WriteLine("\nInsert options:\n");
+
+            for (chosen = InsertOptions.FIRST_MENU_OPTION; chosen <= InsertOptions.LAST_MENU_OPTION; ++chosen)
+                Console.WriteLine("{0,10}\t--\t" + InsertMenuOptionLine[(int)chosen], (int)chosen);
+
+            Console.Write("\nPlease choose an option: ");
+            if (int.TryParse(Console.ReadLine(), out int s))
+                chosen = (InsertOptions)s;
+            else
+                throw new Exception();
+
+            return chosen;
+        }
+
+        public static UpdateOptions PrintUpdateMenu()
+        {
+            UpdateOptions chosen;
+
+            Console.WriteLine("\nUpdate options:\n");
+
+            for (chosen = UpdateOptions.FIRST_MENU_OPTION; chosen <= UpdateOptions.LAST_MENU_OPTION; ++chosen)
+                Console.WriteLine("{0,10}\t--\t" + UpdateMenuOptionLine[(int)chosen], (int)chosen);
+
+            Console.Write("\nPlease choose an option: ");
+            if (int.TryParse(Console.ReadLine(), out int s))
+                chosen = (UpdateOptions)s;
+            else
+                throw new Exception();
+
+            return chosen;
+        }
+
+        public static DisplayOptions PrintDisplayMenu()
+        {
+            DisplayOptions chosen;
+
+            Console.WriteLine("\nDisplay options:\n");
+
+            for (chosen = DisplayOptions.FIRST_MENU_OPTION; chosen <= DisplayOptions.LAST_MENU_OPTION; ++chosen)
+                Console.WriteLine("{0,10}\t--\t" + DispalyMenuOptionLine[(int)chosen], (int)chosen);
+
+            Console.Write("\nPlease choose an option: ");
+            if (int.TryParse(Console.ReadLine(), out int s))
+                chosen = (DisplayOptions)s;
+            else
+                throw new Exception();
+
+            return chosen;
+        }
+
+        public static ListViewOptions PrintListViewMenu()
+        {
+            ListViewOptions chosen;
+
+            Console.WriteLine("\nList View options:\n");
+
+            for (chosen = ListViewOptions.FIRST_MENU_OPTION; chosen <= ListViewOptions.LAST_MENU_OPTION; ++chosen)
+                Console.WriteLine("{0,10}\t--\t" + ListViewMenuOptionLine[(int)chosen], (int)chosen);
+
+            Console.Write("\nPlease choose an option: ");
+            if (int.TryParse(Console.ReadLine(), out int s))
+                chosen = (ListViewOptions)s;
+            else
+                throw new ArgumentException();
+
+            return chosen;
+        }
+
+    }
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -7,8 +7,159 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-           Class1 s;
+            MenuOptions.OpeningOptions ch;
 
-        }
+            do
+            {
+                try { ch = MenuOptions.PrintOpeningMenu(); }
+                catch (ArgumentException) { ch = MenuOptions.OpeningOptions.DEFAULT; }
+
+                switch (ch)
+                {
+                    case MenuOptions.OpeningOptions.EXIT:
+                        Console.WriteLine("\ngood bye!");
+                        break;
+
+                    case MenuOptions.OpeningOptions.ADD:
+                        MenuOptions.InsertOptions IChoice;
+
+                        do
+                        {
+                            try { IChoice = MenuOptions.PrintInsertMenu(); }
+                            catch (ArgumentException) { IChoice = MenuOptions.InsertOptions.DEFAULT; }
+
+                            switch (IChoice)
+                            {
+                                case MenuOptions.InsertOptions.BACK:
+                                    Console.WriteLine("\n");
+                                    break;
+                                case MenuOptions.InsertOptions.STATION:
+                                    break;
+                                case MenuOptions.InsertOptions.DRONE:
+                                    break;
+                                case MenuOptions.InsertOptions.CUSTOMER:
+                                    break;
+                                case MenuOptions.InsertOptions.PACKAGE:
+                                    break;
+
+                                default:
+                                    Console.WriteLine("\nERROR: invalid choice\n");
+                                    break;
+                            }
+
+                        } while ((int)IChoice != 0);
+
+                        break;
+
+                    case MenuOptions.OpeningOptions.UPDATE:
+                        MenuOptions.UpdateOptions UChoice;
+
+                        do
+                        {
+                            try { UChoice = MenuOptions.PrintUpdateMenu(); }
+                            catch (ArgumentException) { UChoice = MenuOptions.UpdateOptions.DEFAULT; }
+
+                            switch (UChoice)
+                            {
+                                case MenuOptions.UpdateOptions.BACK:
+                                    Console.WriteLine("\n");
+                                    break;
+                                case MenuOptions.UpdateOptions.ASSOCIATION:
+                                    break;
+                                case MenuOptions.UpdateOptions.PICKING_UP:
+                                    break;
+                                case MenuOptions.UpdateOptions.SUPPLY:
+                                    break;
+                                case MenuOptions.UpdateOptions.CHARGING:
+                                    break;
+                                case MenuOptions.UpdateOptions.RELEASE:
+                                    break;
+
+                                default:
+                                    Console.WriteLine("\nERROR: invalid choice\n");
+                                    break;
+                            }
+
+                        } while ((int)UChoice != 0);
+
+                        break;
+
+                    case MenuOptions.OpeningOptions.PRINT:
+                        MenuOptions.DisplayOptions DChoice;
+
+                        do
+                        {
+                            try { DChoice = MenuOptions.PrintDisplayMenu(); }
+                            catch (ArgumentException) { DChoice = MenuOptions.DisplayOptions.DEFAULT; }
+
+                            switch (DChoice)
+                            {
+                                case MenuOptions.DisplayOptions.BACK:
+                                    Console.WriteLine("\n");
+                                    break;
+                                case MenuOptions.DisplayOptions.STATION:
+                                    break;
+                                case MenuOptions.DisplayOptions.DRONE:
+                                    break;
+                                case MenuOptions.DisplayOptions.CUSTOMER:
+                                    break;
+                                case MenuOptions.DisplayOptions.PACKAGE:
+                                    break;
+
+                                default:
+                                    Console.WriteLine("\nERROR: invalid choice\n");
+                                    break;
+                            }
+
+                        } while ((int)DChoice != 0);
+
+                        break;
+
+                    case MenuOptions.OpeningOptions.PRINT_LISTS:
+                        MenuOptions.ListViewOptions LVChoice;
+
+                        do
+                        {
+                            try { LVChoice = MenuOptions.PrintListViewMenu(); }
+                            catch (ArgumentException) { LVChoice = MenuOptions.ListViewOptions.DEFAULT; }
+
+                            switch (LVChoice)
+                            {
+                                case MenuOptions.ListViewOptions.BACK:
+                                    Console.WriteLine("\n");
+                                    break;
+                                case MenuOptions.ListViewOptions.STATIONS:
+                                    break;
+                                case MenuOptions.ListViewOptions.DRONES:
+                                    break;
+                                case MenuOptions.ListViewOptions.CUSTOMERS:
+                                    break;
+                                case MenuOptions.ListViewOptions.PACKAGES:
+                                    break;
+                                case MenuOptions.ListViewOptions.UNASSIGNED_STATIONS:
+                                    break;
+                                case MenuOptions.ListViewOptions.AVAILABLE_FOR_CHARGING:
+                                    break;
+ 
+                                default:
+                                    Console.WriteLine("\nERROR: invalid choice\n");
+                                    break;
+                            }
+
+                        } while ((int)LVChoice != 0);
+
+                        break;
+
+                    default:
+                        Console.WriteLine("\nERROR: invalid choice\n");
+                        break;
+                }
+
+            }
+            while ((int)ch != 0);
+
+                Console.Read();
+
+            }
     }
 }
