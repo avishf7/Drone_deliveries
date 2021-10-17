107b54bd9d60855e1187a5ee49cfa75d1c1ee1f6
chaim-gootwain <e77050@gmail.com>
2021-10-17 17:39:54 +0300

    rand

--- DAL/DataSource.cs
+++ DAL/DataSource.cs
@@ -9,6 +9,8 @@ namespace DalObject
 {
     class DataSource
     {
+
+        static Random rand=new Random();
         internal static List<Drone> drones = new();
         internal static List<Station> stations = new();
         internal static List<Package> packages = new();
@@ -17,11 +19,27 @@ namespace DalObject
         class config
         {
             internal int PackageIdCounter;
+           
         }
 
         internal static void Initialize()
         {
+            for (int i = 0; i < 5; i++)
+            {
+                drones.Add(new()
+                {
+                    Id = rand.Next(10),
+                    MaxWeight = (Weight)rand.Next(3),
+                    Model = "COMBO AIR 2S\n",
+                    Status = (DroneStatuses)rand.Next(3),
+                    Battery = 100
+                });
 
+            }
+            for (int i = 0; i < 10; i++)
+            {
+
+            }
         }
     }
 }
