f68d55a0bafe2e3f629ebc45d6426bba373f29cf
chaim-gootwain <e77050@gmail.com>
2021-10-17 16:05:03 +0300

    arrays

--- DAL/DalObject.cs
+++ DAL/DalObject.cs
@@ -10,5 +10,18 @@ namespace DalObject
     class DataSource
     {
         internal static Drone[] drones = new Drone[10];
+        internal static Station[] Stations = new Station[5];
+        internal static Customer[] Customers = new Customer[100];
+        internal static package[] packages = new package[1000];
+
+        class Config
+        {
+            internal static 
+            internal static
+            internal static
+            internal static
+
+        }
+
     }
 }
