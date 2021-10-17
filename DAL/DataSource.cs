34885b8c81da867f1d077de663c71f15c3c9cb90
avishay farkash <62483176+avishf7@users.noreply.github.com>
2021-10-17 18:05:16 +0300

    ex 1.4

--- DAL/DataSource.cs
+++ DAL/DataSource.cs
@@ -9,8 +9,13 @@ namespace DalObject
 {
     class DataSource
     {
-
         static Random rand=new Random();
+        static string[] customerNames = new string[10] { "Shlomi", "Meir", "Miki", "Shalom", "Dani", "Avishay", "Chaim", "Moti", "Shimon", "Bibi" };
+        static string[] phones = new string[10] { "0546273849", "0546223849", "0546211849", "0546413849", "0546254849", "0546273878", "0547273849", "0546273749", "0546277849", "0546273847" };
+        static string[] stationNames = new string[4] { "Electric Charging Station", "Gnrgy Charging Station", "VIRTA Charging Station", "EV-Edge Charging Station"};
+        static string[] models = new string[3] { "MAVIC MINI 2", "Mavic Air 2", "COMBO AIR 2S"};
+
+
         internal static List<Drone> drones = new();
         internal static List<Station> stations = new();
         internal static List<Package> packages = new();
@@ -30,10 +35,10 @@ namespace DalObject
                 {
                     Id = rand.Next(10),
                     MaxWeight = (Weight)rand.Next(3),
-                    Model = "COMBO AIR 2S\n",
+                    Model = models[rand.Next(3)],
                     Status = (DroneStatuses)rand.Next(3),
-                    Battery = 100
-                });
+                    Battery = 90
+                }) ;
 
             }
             for (int i = 0; i < 10; i++)
--- DAL/Station.cs
+++ DAL/Station.cs
@@ -12,16 +12,15 @@ namespace IDAL
         {
             public int Id { get; set; }
             public string Name { get; set; }
-            public int ChargeSlots { get; set; }
+            public int FreeChargeSlots { get; set; }
             public double Longitude { get; set; }
             public double Lattitude { get; set; }
-            public int freePositions { get; set; }
+
 
             public override string ToString()
             {
-                return "Details of ID :" + Id + "\nName: " + Name + "\nCharge slots: " +
-                     "\nLongitude: " + Longitude + "\nLattitude: " + Lattitude +
-                     "\nFree positions: " + freePositions + "\n";
+                return "Details of ID :" + Id + "\nName: " + Name + "\nFree charge slots: " + FreeChargeSlots
+                     + "\nLongitude: " + Longitude + "\nLattitude: " + Lattitude + "\n";
             }
         }
     }
