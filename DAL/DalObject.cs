bd9ba76610cf53a75f77d15aa5c4fe4fdc47e68c
chaim-gootwain <e77050@gmail.com>
2021-10-19 00:33:33 +0300

    1.9

---
+++ DAL/DalObject.cs
@@ -0,0 +1,21 @@
+﻿using IDAL.DO;
+using System;
+using System.Collections.Generic;
+
+namespace DalObject
+{
+	public class DalObject
+	{
+		 List<DroneCharge> droneCharges = new();
+
+		public void AddDrone()
+        {
+            Console.WriteLine("Enter drones ID: ");
+			int id = int.Parse(Console.ReadLine());
+
+
+
+			DataSource.drones.Add(new() {Id = id,  });
+        }
+	}
+}
--- DAL/DataSource.cs
+++ DAL/DataSource.cs
@@ -8,14 +8,13 @@ using IDAL.DO;
 namespace DalObject
 {
 
-    class DataSource
+     class DataSource
     {
         static Random rand = new Random();
         static string[] customerNames = new string[10] { "Shlomi", "Meir", "Miki", "Shalom", "Dani", "Avishay", "Chaim", "Moti", "Shimon", "Bibi" };
         static string[] phones = new string[10] { "0546273849", "0546223849", "0546211849", "0546413849", "0546254849", "0546273878", "0547273849", "0546273749", "0546277849", "0546273847" };
         static string[] stationNames = new string[4] { "Electric Charging Station", "Gnrgy Charging Station", "VIRTA Charging Station", "EV-Edge Charging Station" };
         static string[] models = new string[3] { "MAVIC MINI 2", "Mavic Air 2", "COMBO AIR 2S" };
-
 
         internal static List<Drone> drones = new();
         internal static List<Station> stations = new();
@@ -90,12 +89,15 @@ namespace DalObject
                     DroneId = GetAvailableDrone().Id
 
                 });
+
+                Config config = new()
+                {
+                    PackageIdCounter = packages.Max(pck => pck.Id) + 1
+                };
             }
 
-                    DroneId = drones[rand.Next(5)].Id
 
-                });
-            }
+                
 
             static Drone GetAvailableDrone()
             {
@@ -104,6 +106,8 @@ namespace DalObject
                 {
                     drone = drones[rand.Next(5)];
                 } while (drone.Status != DroneStatuses.AVAILABLE);
+
+                return drone;
             }
 
 
