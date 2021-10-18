fbaf0eabbd0872c65564b3f32b751edfe748bde4
avishay farkash <62483176+avishf7@users.noreply.github.com>
2021-10-18 11:53:27 +0300

    ex 1.6

--- DAL/DataSource.cs
+++ DAL/DataSource.cs
@@ -7,26 +7,32 @@ using IDAL.DO;
 
 namespace DalObject
 {
+
     class DataSource
     {
-        static Random rand=new Random();
+        static Random rand = new Random();
         static string[] customerNames = new string[10] { "Shlomi", "Meir", "Miki", "Shalom", "Dani", "Avishay", "Chaim", "Moti", "Shimon", "Bibi" };
         static string[] phones = new string[10] { "0546273849", "0546223849", "0546211849", "0546413849", "0546254849", "0546273878", "0547273849", "0546273749", "0546277849", "0546273847" };
-        static string[] stationNames = new string[4] { "Electric Charging Station", "Gnrgy Charging Station", "VIRTA Charging Station", "EV-Edge Charging Station"};
-        static string[] models = new string[3] { "MAVIC MINI 2", "Mavic Air 2", "COMBO AIR 2S"};
+        static string[] stationNames = new string[4] { "Electric Charging Station", "Gnrgy Charging Station", "VIRTA Charging Station", "EV-Edge Charging Station" };
+        static string[] models = new string[3] { "MAVIC MINI 2", "Mavic Air 2", "COMBO AIR 2S" };
 
 
         internal static List<Drone> drones = new();
         internal static List<Station> stations = new();
         internal static List<Package> packages = new();
         internal static List<Customer> customers = new();
-
-        class config
+        /// <summary>
+        /// 
+        /// </summary>
+        internal class Config
         {
             internal int PackageIdCounter;
-           
+
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         internal static void Initialize()
         {
 
@@ -81,23 +87,29 @@ namespace DalObject
                     Scheduled = DateTime.Now.AddDays(rand.Next(3)),
                     PickedUp = DateTime.Now.AddHours(rand.Next(3)),
                     Delivered = DateTime.Now.AddHours(rand.Next(4)),
-
-                    DroneId = drones[rand.Next(5)].Id
+                    DroneId = GetAvailableDrone().Id
 
                 });
             }
 
-            static Drone GetAvailableDrone()
-            {
-                Drone drone;
-                do
-                {
-                    drone = drones[rand.Next(5)];
-                } while (drone.Status != DroneStatuses.AVAILABLE);
-            }
-
+            Config config = new() { PackageIdCounter = packages.Max(pck => pck.Id) + 1 };
 
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        static Drone GetAvailableDrone()
+        {
+            Drone drone;
+            do
+            {
+                drone = drones[rand.Next(5)];
+            } while (drone.Status != DroneStatuses.AVAILABLE);
+
+            return drone;
+        }
     }
-  
+
 }
