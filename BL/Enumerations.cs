469f8343f3c235c3ab781c91baba24fe5259d61b
chaim-gootwain <e77050@gmail.com>
2021-10-28 01:33:21 +0300

    begin targil 2

---
+++ BL/Enumerations.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBL.BO
+{
+    class Enumerations
+    {
+        /// <summary>
+        /// Weight types
+        /// </summary>
+        public enum Weight { LIGHT, MEDIUM, HEAVY }
+        /// <summary>
+        /// Situations where the drone can be
+        /// </summary>
+        public enum DroneStatuses { AVAILABLE, MAINTENANCE, DELIVERY }
+        /// <summary>
+        /// Delivery priority
+        /// </summary>
+        public enum Priorities { NORMAL, FAST, EMERENCY }
+    }
+}
