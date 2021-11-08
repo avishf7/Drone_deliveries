1a8104e00630a089736e0fe4c16b42b0ba087e52
chaim-gootwain <e77050@gmail.com>
2021-11-08 21:10:14 +0200

    2.93

---
+++ BL/StationToList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    class StationToList
+    {
+        /// <summary>
+        /// Gets the id
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Gets the name.
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int SeveralAvailableChargingStations { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int NumberOfChargingStationsOccupied { get; set; }
+
+
+        public override string ToString()
+        {
+            return "Details of ID :" + Id + "\nName: " + Name + "\nSeveral available charging stations: "
+                + SeveralAvailableChargingStations + "\nNumber of charging stations occupied: " +
+                NumberOfChargingStationsOccupied + "\n";
+    }
+}
