158ff5b62864560c0e6416a73f59d5b93a39b9d2
avishay farkash <62483176+avishf7@users.noreply.github.com>
2022-01-03 18:22:14 +0200

    fix

--- PL/Windows/Station.xaml.cs
+++ PL/Windows/Station.xaml.cs
@@ -156,7 +156,7 @@ namespace PL.Windows
         {
             if (UpdateName.Text != ""  )
             {
-                bl.UpdateStation(station.Id, UpdateName.Text, station.FreeChargeSlots);
+                bl.UpdateStation(POStation.Id, UpdateName.Text, POStation.FreeChargeSlots);
               //  this.sender.Filtering();
                 MessageBox.Show("Updating the element was completed successfully!", "Notice", MessageBoxButton.OK, MessageBoxImage.Information);
 
